4fabe3e1f0fcc0f0f1aa7922a56f234ee7720420
erwne <erwan.nea@ynov.com>
2025-06-13 15:24:25 +0200

    feat(auth) : login

--- apps/auth-service/src/auth.controller.ts
+++ apps/auth-service/src/auth.controller.ts
@@ -1,32 +1,17 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import {MessagePattern} from "@nestjs/microservices";
-import {JwtService} from "@nestjs/jwt";
-import {UtilisateurClientService} from "./utilisateur-client.service";
-import * as bcrypt from 'bcrypt';
-import {UtilisateurModel} from "./models/utilisateur.model";
+import {LoginDto} from "./dto/login.dto";
 
 @Controller()
 export class AuthController {
 
-  constructor(private readonly authService: AuthService,
-              private readonly jwtService: JwtService,
-              private readonly utilisateurClientService: UtilisateurClientService) {}
+  constructor(private readonly authService: AuthService,) {}
 
-
-  @MessagePattern({ cmd: 'auth_login' })
-  async handleLogin({ email, password }: { email: string; password: string }) {
-    const utilisateur: UtilisateurModel = await this.utilisateurClientService.utilisateurParEmail(email);
-
-    const isValid = await bcrypt.compare(password, utilisateur.password);
-    if (!isValid) {
-      throw new Error('Invalid credentials');
-    }
-
-    const payload = { sub: utilisateur.id, email: utilisateur.email };
-    const token = this.jwtService.sign(payload);
-
-    return { access_token: token };
+  @MessagePattern('auth.login')
+  async login(data: LoginDto) {
+    console.log(data);
+    return this.authService.login(data);
   }
 
   @MessagePattern('auth.verify')
