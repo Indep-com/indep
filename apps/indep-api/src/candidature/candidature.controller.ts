801300fc93eb7d51f91a3c2711e053e4c252ef04
Adel djahnit <adeldjahnita@gmail.com>
2025-06-23 16:55:42 +0200

    feat(candidature): ok CI front

--- apps/indep-api/src/candidature/candidature.controller.ts
+++ apps/indep-api/src/candidature/candidature.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Param, Body, Patch, Delete } from '@nestjs/common';
 import { CandidatureProxyService } from './candidature-proxy.service';
-import { CreateCandidatureDto } from './dto/create-candidature.dto';
+import { CreateCandidatureDto } from '../candidature/dto/create-candidature.dto';
 
 @Controller('candidatures')
 export class CandidatureController {
